9639724acbacc953ebf97ecdf28d9568856be44b
Mateus Brandão <mateusfleria@gmail.com>
2023-05-20 10:13:19 -0300

    Configurando autenticação no mobile, e configurando roteamento no mobile

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,10 @@
+import * as SecureStore from 'expo-secure-store';
+
 import { StatusBar } from 'expo-status-bar'
 import { ImageBackground, View, Text, TouchableOpacity } from 'react-native'
+import { styled } from 'nativewind'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import { useEffect } from 'react'
 
 import {
   useFonts,
@@ -9,20 +14,59 @@ import {
 
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
-import blurBg from './src/assets/bg-blur.png'
-import Stripes from './src/assets/stripes.svg'
-import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
 
-import { styled } from 'nativewind'
+import { api } from '../src/lib/api'
+import { useRouter } from 'expo-router';
 
 const StyledStripes = styled(Stripes)
 
+const GITHUB_CLIENT_ID = 'f196524f232b1a697fb9';
+
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint: `https://github.com/settings/connections/applications/${GITHUB_CLIENT_ID}`,
+};
+
 export default function App() {
+  const router = useRouter();
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  const [request, response, signInWithGithub] = useAuthRequest({
+    clientId: GITHUB_CLIENT_ID,
+    scopes: ['identity'],
+    redirectUri: makeRedirectUri({
+      scheme: 'nlwspacetime'
+    }),
+  },discovery);
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params;
+
+      handleGithubOAuthCode(code);
+    }
+  }, [response]);
+
+  async function handleGithubOAuthCode(code: string){
+    const response = await api.post('/register', {
+      code, isMobile: true
+    });
+    
+    const { token } = response.data
+    
+    await SecureStore.setItemAsync('token', token);
+
+    router.push('/memories');
+  }
 
   if (!hasLoadedFonts) {
     return null
@@ -56,6 +100,7 @@ export default function App() {
         <TouchableOpacity
           className="rounded-full bg-green-500 px-5 py-2"
           activeOpacity={0.7}
+          onPress={() => signInWithGithub()}
         >
           <Text className="font-alt text-sm uppercase text-black">
             Cadastrar lembrança
---
+++ app/memories.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+export default function Memories(){
+  return (
+    <View className="flex-1 justify-center items-center bg-gray-900 px-8 py-10">
+      <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+        Memories
+      </Text>
+    </View>
+  );
+}
